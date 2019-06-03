bff6cfa44a9a20c65173896c2dca3e1ede1c94fe
JavaPsyh <psyhjava@gmail.com>
2019-06-03 18:14:38 +0300

    correct robot

--- src/tasks/robot/MoveRobot.java
+++ src/tasks/robot/MoveRobot.java
@@ -2,37 +2,26 @@ package tasks.robot;
 
 public class MoveRobot {
 
-    private static int horizontalDistance, verticalDistance;
-
-    private static int dementionOfWolking(Direction direction) {
-        return direction == Direction.DOWN || direction == Direction.UP ? verticalDistance : horizontalDistance;
-    }
-
-    private static void wolker(Robot bobot, int distance) {
-        for (; distance > 0; distance--) {
-            bobot.stepForward();
-        }
-    }
-
     public static void moveRobot(Robot robot, int toX, int toY) {
-        horizontalDistance = robot.getX() > toX ? robot.getX() - toX : toX - robot.getX();
-        verticalDistance = robot.getY() > toY ? robot.getY() - toY : toY - robot.getY();
-        Direction longWay = horizontalDistance > verticalDistance ?
-                robot.getX() > toX ? Direction.LEFT : Direction.RIGHT :
-                robot.getY() > toY ? Direction.DOWN : Direction.UP;
-        Direction shortWay = horizontalDistance < verticalDistance ?
-                robot.getX() > toX ? Direction.LEFT : Direction.RIGHT :
-                robot.getY() > toY ? Direction.DOWN : Direction.UP;
-        Direction robotDirection;
-        while (robot.getDirection() != longWay) {
+
+        int startX = robot.getX();
+        int startY = robot.getY();
+        Direction directionToX = startX > toX ? Direction.LEFT : Direction.RIGHT;
+        Direction directionToY = startY > toY ? Direction.DOWN : Direction.UP;
+        int distanceX = startX > toX ? startX - toX : toX - startX;
+        int distanceY = startY > toY ? startY - toY : toY - startY;
+
+        while (robot.getDirection() != directionToX) {
             robot.turnLeft();
         }
-        robotDirection = longWay;
-        wolker(robot, dementionOfWolking(robotDirection));
-        while (robot.getDirection() != shortWay) {
-            robot.turnRight();
+        for (int x = distanceX; x > 0; x--) {
+            robot.stepForward();
         }
-        robotDirection = shortWay;
-        wolker(robot, dementionOfWolking(robotDirection));
+        while (robot.getDirection() != directionToY) {
+            robot.turnLeft();
+        }
+        for (int y = distanceY; y > 0; y--) {
+            robot.stepForward();
+        }
     }
 }
